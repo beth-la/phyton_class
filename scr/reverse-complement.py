e9e7ed54ec7162c48199c13be1040bc8dae682e5
Ulises Plascencia <ulisesp@lcg.unam.mx>
2022-02-24 12:12:41 -0600

    no des lata

--- scr/reverse-complement.py
+++ scr/reverse-complement.py
@@ -6,8 +6,7 @@ VERSION
     1.0
     
 AUTHOR
-	Victor Ulises Plascencia Perez  sexy
-    
+	Victor Ulises Plascencia Perez  No. Cuenta: 421016633    
 DESCRIPTION
 	Make the reverse complement of DNA sequence
     
@@ -18,7 +17,7 @@ USAGE
 
     % python reverse-complement.py -i FILENAME
     
-    example
+	This is an example    example
     
     % python reverse-complement -i sequence.txt
 
