be4707df2d1ae770fb614b5db98f301e589376bd
beth-la <100377667+beth-la@users.noreply.github.com>
2022-03-30 18:44:03 -0600

    Primera version
    Primera versión del programa con las condiciones establecidas

--- scr/Contador_nucleotidos_input.py
+++ scr/Contador_nucleotidos_input.py
@@ -1,5 +1,5 @@
 # Name: Contador de nucleotidos con input.
-# Version:
+# Version: 1.0
 # Author: Lopez A. Brenda E.
 # Descripcion: Programa que dada una secuencia de ADN mediante un input, cuenta la frecuencia de los nucleotidos.
 # Category: 
@@ -8,9 +8,10 @@
 # See also:
 
 # Pedimos al usuario ingrese una secuencia de ADN 
+# La secuencia  solicitada será guaradada en la variable ADN
 
 ADN= input("Introduce una secuencia de ADN: ")
 
 # Tomando la secuencia guardada en la variable ADN, utilizamos el metodo count para contar la aparicion de cada letra dentro de la secuencia. 
 
-print(f"La secuencia tiene estos nucleotidos: A:{ADN.count('A')} C:{ADN.count('C')} T:{ADN.count('T')} G:{ADN.count('G')}")
+print(f"El total por base es: A:{ADN.count('A')} C:{ADN.count('C')} T:{ADN.count('T')} G:{ADN.count('G')}")
