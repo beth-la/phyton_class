18c28418321bf5db72e68f3d3e537f9c98e6cf5b
beth-la <100377667+beth-la@users.noreply.github.com>
2022-03-30 19:40:46 -0600

    Primera versión del programa

--- scr/generador_fasta.py
+++ scr/generador_fasta.py
@@ -1,20 +1,21 @@
-# Name: Generador archivo fasta
-# Version:
+# Name: Generador de un archivo fasta
+# Version: 1.0 
 # Author: Lopez A. Brenda E.
-# Descripcion: Programa que genera un archivo fasta a partir de un archivo de ADN.
+# Descripcion: Programa que genera un archivo fasta a partir de un archivo con extension .txt que contiene una secuencia de ADN.
 # Category: 
 # Usage:
 # Arguments:
 # See also:
 
-# Abrir el archivo con with open.
-# La variable ADN guarda el contenido del archivo dna.txt
+# Abrir el archivo con with open cierra automaticamente el archvio. 
+# Se guarda el contenido del archivo utilizando el metodo .read() en la variable ADN
 
 with open('data/dna.txt','r') as archivo:
     ADN= archivo.read()
 
-# Creamos un nuevo archivo que tendra el formato fasta:
-# Escribimos el encabezado y la secuencia.
+# Creamos un nuevo archivo que tendra el formato fasta, abriendolo como "write"
+# Escribimos el encabezado y la secuencia con .write
+# Cerramos el arhivo. 
 
 my_file= open("data/dna.fasta","w")
 my_file.write(">sequence_name \n")
